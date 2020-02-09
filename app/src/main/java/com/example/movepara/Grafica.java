cd28bc407d2246a237158a4f5aa2d4891c6916ea
r0mell <r0mell@users.noreply.github.com>
2020-02-09 01:14:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/movepara/Grafica.java
@@ -0,0 +1,84 @@
+package com.example.movepara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+
+import static java.lang.Thread.*;
+
+public class Grafica extends AppCompatActivity {
+
+
+    private TextView coorX;
+    private TextView coorY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grafica);
+        LineGraphSeries<DataPoint> series;
+
+        ArrayList<String> coordenadaX = getIntent().getStringArrayListExtra("coorX");
+        ArrayList<String> coordenadaY = getIntent().getStringArrayListExtra("coorY");
+        String distanciaX = getIntent().getStringExtra("alcance");
+
+        coordenadaX.add(distanciaX);
+        coordenadaY.add("0");
+
+        //double maxX =  Double.parseDouble(distanciaX);
+
+        for (int x = 0; x < coordenadaX.size(); x++) {
+            Log.i("", "distancia X=  " + coordenadaX.get(x));
+            Log.i("", "distancia Y=  " + coordenadaY.get(x));
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        series = new LineGraphSeries<DataPoint>();
+
+        for (int i = 0 ;i<coordenadaX.size(); i++){
+            series.appendData(new DataPoint(Double.parseDouble(coordenadaX.get(i)),Double.parseDouble(coordenadaY.get(i))),true,500);
+        }
+
+        graph.addSeries(series);
+
+        coorX = findViewById(R.id.txtCoorX);
+        coorY = findViewById(R.id.txtCoorY);
+
+        coorX.setText(coordenadaX.get(coordenadaX.size()-1));
+        coorY.setText(coordenadaY.get(coordenadaY.size()-1));
+
+    }
+
+    public void regresar(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
+    }
+
+    public void actualizar(ArrayList<String> coordenadaX, ArrayList<String> coordenadaY) {
+
+        TextView coorX;
+        TextView coorY;
+
+        coorX = findViewById(R.id.txtCoorX);
+        coorY = findViewById(R.id.txtCoorY);
+
+        for (int i = 0; i < coordenadaX.size(); i++) {
+            coorX.setText(coordenadaX.get(i));
+            coorY.setText(coordenadaY.get(i));
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/movepara/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.movepara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.lang.Math;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static int GRAVEDAD = -10;
+    private EditText val;
+    private EditText ang;
+
+    private TextView alc;
+    private TextView alt;
+    private TextView tT;
+
+    private TableLayout tableLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public double calculoAltura(double velocidad, double angulo) {
+
+        double altura = 0;
+
+        altura = (Math.pow(velocidad, 2)) * (Math.pow(Math.sin(angulo * (Math.PI / 180)), 2)) / (2 * -GRAVEDAD);
+
+        Log.i("", "mi nombre es " + altura);
+
+        return altura;
+    }
+
+    public double calcularAlcance(double velocidad, double angulo) {
+        double alcance = 0;
+
+        alcance = (Math.pow(velocidad, 2) * Math.sin((2 * angulo) * (Math.PI / 180))) / -GRAVEDAD;
+        return alcance;
+    }
+
+    public double tiempoTotal(double alcance, double velocidadX) {
+        double tTotal;
+
+        tTotal = alcance / velocidadX;
+
+        return tTotal;
+    }
+
+    public void calculos(View view) {
+
+        double altura;
+        double alcance;
+        double tTotal;
+        double velocidadX;
+        double velocidadY;
+
+        val = findViewById(R.id.txtVelocidad);
+        double velocidad = Double.parseDouble(val.getText().toString());
+
+        ang = findViewById(R.id.txtAngulo);
+        double angulo = Double.parseDouble(ang.getText().toString());
+        velocidadX = Math.cos(angulo * (Math.PI / 180)) * velocidad;
+        velocidadY = Math.sin(angulo * (Math.PI / 180)) * velocidad;
+
+        altura = calculoAltura(velocidad, angulo);
+        alcance = calcularAlcance(velocidad, angulo);
+        tTotal = tiempoTotal(alcance, velocidadX);
+
+        alc = findViewById(R.id.txtAltura);
+        alc.setText(String.format("%.4f", altura) + " metros");
+
+        alt = findViewById(R.id.txtAlcance);
+        alt.setText(String.format("%.4f", alcance) + " metros");
+
+        tT = findViewById(R.id.txtTiempoT);
+        tT.setText(String.format("%.4f", tTotal) + " segundos");
+
+
+        graficar(alcance, altura, tTotal, velocidadX, velocidadY);
+
+    }
+
+    //esta funcion permite calcular el punto en Y para cada punto X
+    public double calcularH(double tiempo, double velocidadY) {
+
+        double respuesta = 0;
+        respuesta = (velocidadY * tiempo) + ((GRAVEDAD * (tiempo * tiempo)) / 2);
+
+        return respuesta;
+    }
+
+    public void graficar(double alcance, double altura, double tTotal, double velocidadX, double velocidadY) {
+
+        double temp = 0;
+        double distX = 0;
+        double distY = 0;
+        ArrayList<String> coordenadaX = new ArrayList<String>();
+        ArrayList<String> coordenadaY = new ArrayList<String>();
+
+        while (temp <= tTotal) {
+
+            //Con esta funcion se generan los segmentos del eje x aplicando un porcentaje para cualquier distancia
+            distX = temp * velocidadX;
+            distY = calcularH(temp, velocidadY);
+            coordenadaX.add(String.valueOf(distX));
+            coordenadaY.add(String.valueOf(distY));
+            //Log.i("", "distancia X=  " + distX + "distancia Y=  " + distY + "tiempo= " + temp);
+            temp = temp + (tTotal * 0.02);
+        }
+        Button button = (Button) findViewById(R.id.btnGraficar);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                enviar(coordenadaX,coordenadaY,alcance);
+
+            }
+        });
+
+
+        //muestro las coordenadas generadsa y guardadas en las listas
+        /*for(int x=0;x<coordenadaX.size();x++) {
+            Log.i("", "distancia X=  "+coordenadaX.get(x));
+            Log.i("", "distancia Y=  "+coordenadaY.get(x));
+        }*/
+    }
+
+    public void enviar(ArrayList<String> coordenadaX,ArrayList<String> coordenadaY,double alcance) {
+
+        Intent i = new Intent(this, Grafica.class);
+        i.putStringArrayListExtra("coorX", coordenadaX);
+        i.putStringArrayListExtra("coorY", coordenadaY);
+        i.putExtra("alcance",String.valueOf( alcance));
+        startActivity(i);
+    }
+}
+
+
